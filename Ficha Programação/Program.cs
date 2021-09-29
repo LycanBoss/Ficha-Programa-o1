6a670f14462a918a9e87f6806c8a53714704a1fb
Adrian Gabriel <lycanboss1995@gmail.com>
2021-09-29 11:00:51 -0300

    Adicionar arquivos de projeto.

---
+++ Ficha Programação/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ficha_Programação1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float celcius , fahrenheit;
+            Console.WriteLine("Escreva uma temperatura em Celsius para ser convertido em Fahrenheit");
+            celcius = float.Parse(Console.ReadLine());
+            fahrenheit = (celcius * 9 / 5) + 32;
+            Console.WriteLine("Sua temperatura convertida em Fahrenheit é:{0}", fahrenheit);
+            Console.ReadKey();
+        }
+    }
+}
